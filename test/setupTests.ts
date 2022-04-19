9402336992a5f6667e083f7d195d923acf9edef2
Christian de Barrio <christian.debarrio@cabify.com>
2022-04-19 19:00:57 +0200

    update to react 18

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,15 +3,13 @@ import 'isomorphic-unfetch';
 
 import { server } from './mockServer';
 
-beforeAll(() =>
-  server.listen({
-    onUnhandledRequest: 'warn',
-  }),
-);
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
+
 afterEach(() => {
   server.resetHandlers();
   server.events.removeAllListeners('request:start');
   server.events.removeAllListeners('request:match');
   server.events.removeAllListeners('request:unhandled');
 });
+
 afterAll(() => server.close());
