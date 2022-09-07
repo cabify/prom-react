3a37505007d33a7de6ae19c98ae07ffbadf78d61
Alejandro Gallardo Escobar <alejandro.gallardo@cabify.com>
2022-09-07 09:31:01 +0200

    Fix format

--- src/MetricsLogger.tsx
+++ src/MetricsLogger.tsx
@@ -11,7 +11,7 @@ interface MetricsLoggerProps {
   /**
    * If set, it will replace the original logger. Ensure the reference of this value doesn't change between renders by memoizing the logger function or defining it outside the component to avoid performance issues.
    */
-  logger?: ObserveCallback
+  logger?: ObserveCallback;
 }
 
 const MetricsLogger = ({ logger = defaultLogger }: MetricsLoggerProps) => {
