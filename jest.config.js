e7ccfb07a410df795338c9f4785c620b6cf0ca6d
Alejandro Frias <alejandro.frias@cabify.com>
2023-01-03 08:23:25 +0100

    remove unneeded files

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,6 @@ module.exports = {
   testEnvironmentOptions: {
     url: 'http://localhost/',
   },
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/fileMock.js',
-    '^.*\\.scss$': '<rootDir>/test/styleMock.js',
-  },
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   testMatch: ['<rootDir>/src/**/*.spec.(ts|tsx)'],
   transform: {
